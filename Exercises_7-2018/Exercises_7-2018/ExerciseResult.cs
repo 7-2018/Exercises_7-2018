39edb645275f3c62e5c79eb87ed4c18457a127fa
DESKTOP-E66HHQA\Marta <marta.m2000@yahoo.com>
2020-11-05 19:42:17 +0100

    Commit 3- Model klasa kreirana

---
+++ Exercises_7-2018/Exercises_7-2018/ExerciseResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_7_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndex, int points)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            Points = points;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+    }
+
+}
